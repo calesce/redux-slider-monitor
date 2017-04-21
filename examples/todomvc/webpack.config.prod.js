0efe7ea9fc4a9f1c3d8d97501e7a9dda399c4ff6
Jhen <developer@jhen.me>
2017-04-21 14:52:29 +0800

    Add deploy script for gh-pages

--- examples/todomvc/webpack.config.prod.js
+++ examples/todomvc/webpack.config.prod.js
@@ -2,29 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    host: 'localhost',
-    port: process.env.PORT || 3000,
-    historyApiFallback: true,
-    hot: true
-  },
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './index'
-  ],
+  entry: './index',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     })
   ],
   resolve: {
